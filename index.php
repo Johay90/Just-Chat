004d69b1cc843ef9bbb4b98f8f910ee5e8b1c102
Johay90 <johay.jjj@googlemail.com>
2015-06-03 14:19:19 +0100

    Draft design
    VERY EARLY DRAFT - Good chance scrapping this design

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Live Chat Platform</title>
+  <link rel="Stylesheet" href="style.css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+  <div id="outer-box">
+
+    <div id="online-bar"><p>Current Online Users: x</p></div>
+
+    <div id="name-row">
+      <p>Johay (hh:mm)</p>
+      <p>Johay (hh:mm)</p>
+      <p>Johay (hh:mm)</p>
+      <p>Johay (hh:mm)</p>
+      <p>Johay (hh:mm)</p>
+
+    </div>
+
+    <div id="content-row">
+
+    </div>
+
+    <div id="post-text"></div>
+
+</div>
+
+</body>
+
+</html>
